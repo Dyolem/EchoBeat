e6d2b568f38ba712f4e5147d419570b7f40dba52
Adam Davis <dyolem9@gmail.com>
2024-12-07 21:28:36 +0800

    feat:Build the initial structure of the chromatic component

--- src/views/daw/midi-editor/midi-sidebar/chromatic-scale/index.vue
+++ src/views/daw/midi-editor/midi-sidebar/chromatic-scale/index.vue
@@ -1,13 +1,52 @@
-<script setup></script>
+<script setup>
+import Octave from "@/views/daw/midi-editor/midi-sidebar/chromatic-scale/octave/index.vue"
+import { computed, ref } from "vue"
+const chromaticScaleArr = ref(["C1", "C2", "C3", "C4", "C5", "C6", "C7"])
+const octaveInfoArr = computed(() => {
+  return chromaticScaleArr.value
+    .slice()
+    .reverse()
+    .map((item) => {
+      return { id: item, octaveName: item }
+    })
+})
+</script>
 
 <template>
-  <div class="chromatic-scale-container">chromatic-scale-container</div>
+  <div class="chromatic-scale-container">
+    <div class="fold-button">Fold</div>
+    <div class="octave-boxes beatified-scrollbar">
+      <Octave
+        v-for="item in octaveInfoArr"
+        :key="item.id"
+        :id="item.id"
+        :octave-name="item.octaveName"
+      ></Octave>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+@import "src/styles/scrollbar.css";
 .chromatic-scale-container {
   width: 100px;
   height: 100%;
   background-color: antiquewhite;
+  display: flex;
+  flex-direction: column;
+}
+.fold-button {
+  width: 100%;
+  height: 50px;
+  flex-shrink: 0;
+  text-align: center;
+  align-content: center;
+  background-color: palevioletred;
+}
+.octave-boxes {
+  flex-grow: 1;
+  overflow: auto;
+  width: 100%;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/views/daw/midi-editor/midi-sidebar/chromatic-scale/octave/index.vue
@@ -0,0 +1,88 @@
+<script setup>
+const props = defineProps({
+  id: {
+    type: Number,
+  },
+  octaveName: {
+    type: String,
+    default: "C",
+  },
+})
+</script>
+
+<template>
+  <div class="octave-box">
+    <div class="upper-tetra-chord tetra-chord">
+      <div class="white-key-block">
+        <div class="white-key">B</div>
+        <div class="white-key">A</div>
+        <div class="white-key">G</div>
+        <div class="white-key">F</div>
+      </div>
+      <div class="black-key-block">
+        <div class="black-key">A#</div>
+        <div class="black-key">G#</div>
+        <div class="black-key">F#</div>
+      </div>
+    </div>
+    <div class="lower-tetra-chord tetra-chord">
+      <div class="white-key-block">
+        <div class="white-key">E</div>
+        <div class="white-key">D</div>
+        <div class="white-key">{{ `${octaveName}` }}</div>
+      </div>
+
+      <div class="black-key-block">
+        <div class="black-key">D#</div>
+        <div class="black-key">C#</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.octave-box {
+  --black-key-height: 10px;
+  --white-key-height: 16px;
+  --white-key-width: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+  background-color: antiquewhite;
+}
+
+.tetra-chord {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.black-key-block {
+  position: absolute;
+  top: calc(var(--white-key-height) - var(--black-key-height) / 2);
+  display: flex;
+  flex-direction: column;
+  gap: calc(var(--white-key-height) - var(--black-key-height));
+}
+
+.white-key {
+  box-sizing: border-box;
+  text-align: end;
+  background-color: white;
+  width: var(--white-key-width);
+  height: 16px;
+  border: 1px solid gray;
+  border-top: none;
+  font-size: 12px;
+}
+.black-key {
+  box-sizing: border-box;
+  width: calc(var(--white-key-width) * 0.618);
+  height: 10px;
+  background-color: black;
+  color: white;
+  font-size: 12px;
+}
+</style>
