13217904d4f8413a1f8c3724ab0a2c80cb0f9df4
Adam Davis <dyolem9@gmail.com>
2025-02-06 18:41:18 +0800

    perl:Optimize data structure

--- src/store/daw/type.js
+++ src/store/daw/type.js
@@ -1,10 +1,14 @@
 // types.js
+
 /**
- * @typedef {[number, number]} ScaleYTuple - 垂直缩放比例范围元组 [最小值, 最大值]
+ * @typedef {string} AudioTrackId - 音轨唯一标识符
+ * @typedef {string} SubTrackItemId
+ * @typedef {string} WorkspaceId
+ * @typedef {string} PitchNameId - 以音名作为Map的键，同时具有id的作用
  */
 
 /**
- * @typedef {string} PitchNameId - 以音名作为Map的键，同时具有id的作用
+ * @typedef {[number, number]} ScaleYTuple - 垂直缩放比例范围元组 [最小值, 最大值]
  */
 
 /**
@@ -34,16 +38,54 @@
 
 /** @typedef {Map<PitchNameId, NoteTrack>} NoteItemsMap */
 
-/**@typedef {Object} WorkspaceMap
- * @property {string} id - 工作区自身唯一ID
+/**
+ * @typedef {Object} Workspace
+ * @property {WorkspaceId} id - 工作区自身唯一ID
  * @property {string} audioTrackId - 工作区所属音轨的唯一ID
+ * @property {number} startPosition - 工作区起始位置
  * @property {NoteItemsMap} noteItemsMap - 工作区内创建的note元素的Map集合
  * @property {number} width - 工作区宽度
- * @property {number} startPosition - 工作区起始位置
+ * @property {string} workspaceBadgeName - 工作区名称
+ * @property {number} zoomRatio - 工作区缩放比例
+ * @property {SubTrackItemId} subTrackItemId - 与工作区映射的子音轨
+ *
+ * @typedef {Map<WorkspaceId,Workspace>} WorkspaceMap
+ */
+
+/**
+ * @typedef {Object} SubTrackItem
+ * @property {SubTrackItemId} subTrackItemId
+ * @property {AudioTrackId} audioTrackId
+ * @property {WorkspaceId} workspaceId
+ * @property {number} trackItemWidth
+ * @property {number} trackItemHeight
+ * @property {string} mainColor
+ * @property {number} startPosition
+ * @property {string} trackName
+ * @property {number} trackZoomRatio
+ */
+/**
+ * @typedef {Map<SubTrackItemId,SubTrackItem>} SubTrackItemsMap
+ */
+
+/**
+ * @typedef {Object} MixTrackUnit
+ * @property {AudioTrackId} id - 音轨的唯一标识符
+ * @property {string} audioTrackName - 音轨显示名称
+ * @property {string} mainColor - 音轨主色（十六进制颜色代码）
+ * @property {number} serialNumbering - 音轨序号
+ * @property {number} startPosition - 音轨起始位置（单位：像素）
+ * @property {SubTrackItemsMap} subTrackItemsMap - 音轨中的子工作区
+ * @property {number} mainEditorZoomRatio - 主编辑区的缩放倍率
+ */
+
+/**
+ * @typedef {Map<AudioTrackId,MixTrackUnit>} MixTracksMap -存储所有音轨的Map集合
  */
 
 /**
  * @typedef {Object} MidiWorkspace
+ * @property {string} workspaceBadgeName
  * @property {Number} zoomRatio - midi编辑区的当前的缩放倍率
  * @property {WorkspaceMap} workspaceMap - 存储midi工作区的Map结构
  */
