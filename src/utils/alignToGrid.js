a1475ca9b5c4d85843a5f4b50b1bff2bd05fd028
Adam Davis <dyolem9@gmail.com>
2025-02-20 22:46:56 +0800

    feat:The grid adsorption calculation function is added

--- src/utils/alignToGrid.js
+++ src/utils/alignToGrid.js
@@ -1,5 +1,4 @@
-import { ALIGN_TYPE } from "@/constants/daw/index.js"
-
+import { ALIGN_TYPE, BASE_GRID_WIDTH } from "@/constants/daw/index.js"
 export function alignToGrid(value, { gridSize, alignType }) {
   if (!ALIGN_TYPE.includes(alignType)) {
     throw new Error(
@@ -18,4 +17,26 @@ export function alignToGrid(value, { gridSize, alignType }) {
     }
   }
   throw new Error(`Invalid parameters`)
+}
+
+export function snapToGrid(
+  value,
+  { gridSize = BASE_GRID_WIDTH, threshold = 5 } = {},
+) {
+  if (
+    typeof value !== "number" ||
+    typeof gridSize !== "number" ||
+    typeof threshold !== "number"
+  ) {
+    return new TypeError('expect "Number" param type')
+  }
+  const gridCount = Math.floor(value / gridSize)
+  const rest = value % gridSize
+  if (gridSize - rest <= threshold) {
+    return (gridCount + 1) * gridSize
+  }
+  if (rest <= threshold) {
+    return gridCount * gridSize
+  }
+  return value
 }
