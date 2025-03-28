afa7f458000559dd11fe068103d0c952dbfc03c8
Adam Davis <dyolem9@gmail.com>
2025-03-28 22:35:26 +0800

    feat:The global menu UI is displayed

--- src/views/daw/header/GlobalMenu.vue
+++ src/views/daw/header/GlobalMenu.vue
@@ -1,35 +1,171 @@
-<script setup></script>
+<script setup>
+import Menu from "@/views/daw/components/context-menu/Menu.vue"
+import { ref } from "vue"
+const globalMenuIconsSet = {
+  GLOBAL_MENU: "material-symbols:menu-rounded",
+  PROJECT_SUB_MENU: "ph:music-note-light",
+  NEW_PROJECT: "material-symbols-light:add-box-outline-rounded",
+  RECENT_PROJECT: "material-symbols:tab-recent",
+  GLOBAL_DOWNLOAD: "ep:download",
+  GLOBAL_EDIT: "fluent:edit-32-regular",
+  UNDO: "octicon:undo-24",
+  REDO: "octicon:redo-24",
+  GLOBAL_TOOLS: "iconoir:angle-tool",
+  CYCLE: "oi:loop",
+  GLOBAL_VIEW: "lets-icons:view",
+  GRID_SIZE: "material-symbols-light:grid-on-outline",
+  FULL_SCREEN: "material-symbols-light:fullscreen-rounded",
+  EXIT_FULL_SCREEN: "material-symbols-light:fullscreen-exit-rounded",
+  GLOBAL_SETTINGS: "tdesign:setting",
+  SNAP_MAGNET: "bxs:magnet",
+}
+const globalMenuData = ref([
+  {
+    value: "global menu",
+    label: "",
+    icon: {
+      name: globalMenuIconsSet.GLOBAL_MENU,
+      style: {
+        fontSize: "24px",
+      },
+    },
+    children: [
+      {
+        value: "project",
+        label: "Project",
+        icon: {
+          name: globalMenuIconsSet.PROJECT_SUB_MENU,
+        },
+        children: [
+          {
+            value: "New Project",
+            label: "New Project",
+            icon: {
+              name: globalMenuIconsSet.NEW_PROJECT,
+            },
+          },
+          {
+            value: "Recent Projects",
+            label: "Recent Projects",
+            icon: {
+              name: globalMenuIconsSet.RECENT_PROJECT,
+            },
+          },
+          {
+            value: "Download",
+            label: "Download",
+            icon: {
+              name: globalMenuIconsSet.GLOBAL_DOWNLOAD,
+            },
+            children: [
+              {
+                value: "Tracks",
+                label: "Tracks",
+              },
+              {
+                value: "Mixdown As",
+                label: "Mixdown As",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        value: "edit",
+        label: "Edit",
+        icon: {
+          name: globalMenuIconsSet.GLOBAL_EDIT,
+        },
+        children: [
+          {
+            value: "undo",
+            label: "Undo",
+            icon: {
+              name: globalMenuIconsSet.UNDO,
+            },
+          },
+          {
+            value: "redo",
+            label: "Redo",
+            icon: {
+              name: globalMenuIconsSet.REDO,
+            },
+          },
+        ],
+      },
+      {
+        value: "tools",
+        label: "Tools",
+        icon: {
+          name: globalMenuIconsSet.GLOBAL_TOOLS,
+        },
+        children: [
+          {
+            value: "cycle",
+            label: "Cycle",
+            icon: {
+              name: globalMenuIconsSet.CYCLE,
+            },
+          },
+        ],
+      },
+      {
+        value: "view",
+        label: "View",
+        icon: {
+          name: globalMenuIconsSet.GLOBAL_VIEW,
+        },
+        children: [
+          {
+            value: "grid size",
+            label: "Grid Size",
+            icon: {
+              name: globalMenuIconsSet.GRID_SIZE,
+            },
+            children: [
+              {
+                value: "Tracks",
+                label: "Tracks",
+              },
+              {
+                value: "Mixdown As",
+                label: "Mixdown As",
+              },
+            ],
+          },
+          {
+            value: "full screen",
+            label: "Full Screen",
+            icon: {
+              name: globalMenuIconsSet.FULL_SCREEN,
+            },
+          },
+        ],
+      },
+      {
+        value: "settings",
+        label: "Settings",
+        icon: {
+          name: globalMenuIconsSet.GLOBAL_SETTINGS,
+        },
+        children: [
+          {
+            value: "snap",
+            label: "Snap To Grid",
+            icon: {
+              name: globalMenuIconsSet.SNAP_MAGNET,
+            },
+          },
+        ],
+      },
+    ],
+  },
+])
+</script>
 
 <template>
   <div class="global-menu-container">
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      :collapse="true"
-      @open="handleOpen"
-      @close="handleClose"
-    >
-      <el-sub-menu index="1">
-        <template #title>
-          <echo-material-symbols:menu-rounded
-            class="menu-icon"
-          ></echo-material-symbols:menu-rounded>
-          <span>Navigator One</span>
-        </template>
-        <el-menu-item-group>
-          <template #title><span>Group One</span></template>
-          <el-menu-item index="1-1">item one</el-menu-item>
-          <el-menu-item index="1-2">item two</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="1-4">
-          <template #title><span>item four</span></template>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
-        </el-sub-menu>
-      </el-sub-menu>
-    </el-menu>
+    <Menu :menu-data="globalMenuData"></Menu>
   </div>
 </template>
 
@@ -40,9 +176,5 @@
   --el-menu-hover-text-color: none;
   --el-menu-hover-bg-color: none;
   border: none;
-}
-.menu-icon {
-  color: #ffffff;
-  font-size: 1.5rem;
 }
 </style>
