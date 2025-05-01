f2736553a3b770c74271d5f1c1d69b50ed79da91
Adam Davis <dyolem9@gmail.com>
2025-05-01 23:05:03 +0800

    feat:Initialize the data from indexeddb when the page is loaded

--- src/core/audio/initAudioResource.js
+++ src/core/audio/initAudioResource.js
@@ -1,15 +1,17 @@
 import { initMetronome } from "@/core/audio/playMetronome.js"
 import { useAudioStore } from "@/store/daw/audio/index.js"
 import { useAudioGeneratorStore } from "@/store/daw/audio/audioGenerator.js"
+import { initDataFromIndexeddb } from "@/core/history/index.js"
 const audioGeneratorStore = useAudioGeneratorStore()
-const { preCreateBuffer } = audioGeneratorStore
+const { initAudioTrackSoundBuffer } = audioGeneratorStore
 const audioStore = useAudioStore()
 export async function initAudioResource(_audioContext) {
   const audioContext = _audioContext || audioStore.audioContext
   try {
+    await initDataFromIndexeddb()
     return await Promise.all([
       initMetronome(audioContext),
-      preCreateBuffer(audioContext),
+      initAudioTrackSoundBuffer(audioContext),
     ])
   } catch (error) {
     throw error
